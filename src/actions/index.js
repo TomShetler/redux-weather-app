3341d3ea083c1d514805a164bbcace14eb57a4b6
ThomasShetler <tshetler93@gmail.com>
2017-07-18 10:34:05 -0400

    Add weather list container to render cities

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,7 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
 
-  console.log('Request is', request);
+  //console.log('Request is', request);
   return {
     type: FETCH_WEATHER,
     payload: request
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import WeatherReducer from './Reducer_Weather';
+import WeatherReducer from './reducer_weather';
 
 const rootReducer = combineReducers({
   weather: WeatherReducer
